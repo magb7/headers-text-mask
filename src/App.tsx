3341acf6fa692034d51392786bae573e73bf4a3f
Margaux Bédu <margaux.bedu@gmail.com>
2022-03-07 16:41:06 +0000

    create first header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <div className="img" />
+      <svg className="svg">
+        <defs>
+          <mask id="mask" x="0" y="0" width="100%" height="100%">
+            <rect id="alpha" x="0" y="0" width="100%" height="100%" />
+            <text x="50%" y="50%" className="text">
+              Forest
+            </text>
+          </mask>
+        </defs>
+        <rect id="base" x="0" y="0" width="100%" height="100%" />
+      </svg>
+    </header>
   );
 }
 
