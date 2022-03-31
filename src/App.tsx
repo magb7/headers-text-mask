79b8da8b943fc9feae66c39804fea91f4572ef9e
Margaux Bédu <margaux.bedu@gmail.com>
2022-03-31 07:03:11 +0000

    add brightness on path header

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      {/* <Forest />
+      <Forest />
       <Mountain />
-      <Path /> */}
+      <Path />
       <Iceland />
     </>
   );
